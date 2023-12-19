f972d37627fefed733bc75ed976e25ac46ae702c
Pranay Kumar <98086497+M-PRANAY@users.noreply.github.com>
2023-12-19 21:53:33 +0530

    Update app.py

--- PythonSample/app.py
+++ PythonSample/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! It me Pranay'
+    return 'Hello, world! It me Pranay Changed'
 
 if __name__ == '__main__':
     app.run()
